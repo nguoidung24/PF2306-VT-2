8d1baeb99f66ee63315b9f37cede690adc98e00d
nguoidung24 <dungtamtithoi88@gmail.com>
2023-10-12 20:20:51 +0700

    Signed-off-by: nguoidung24 <dungtamtithoi88@gmail.com>

---
+++ Day_04/browse-/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    // <div>ok</div>
+  <h4>Browser's details: {navigator.userAgent}</h4>
+  );
+}
+
+export default App;
